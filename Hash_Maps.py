33b4bf239dc8f8eef1a8f73fd66ed36670a91060
Luis Francisco Contreras Gonzalez <51728784+LuisContrerasGlz@users.noreply.github.com>
2021-07-19 00:47:11 -0500

    Update Hash_Maps.py

--- Hash_Maps.py
+++ Hash_Maps.py
@@ -15,3 +15,15 @@
 # Let’s say that the number we correspond with the name “Joan McNeil” is 17. We find the 17th index of the array we’re using to store our map and save the value (Libra) there.
 # How did we get 17, though? We use a special function that turns data like the string “Joan McNeil” into a number. This function is called a hashing function, or a hash function. 
 # Hashing functions are useful in many domains, but for our data structure the most important aspect is that a hashing function returns an array index as output.
+
+# A hash function takes a string (or some other type of data) as input and returns an array index as output. 
+# In order for it to return an array index, our hash map implementation needs to know the size of our array. 
+# If the array we are saving values into only has 4 slots, our hash map’s hashing method should not return an index bigger than that.
+# In order for our hash map implementation to guarantee that it returns an index that fits into the underlying array, the hash function will first compute a value using some scoring metric: this is the hash value, hash code, or just the hash. 
+# Our hash map implementation then takes that hash value mod the size of the array.
+# This guarantees that the value returned by the hash function can be used as an index into the array we’re using.
+
+# It is actually a defining feature of all hash functions that they greatly reduce any possible inputs (any string you can imagine) into a much smaller range of potential outputs (an integer smaller than the size of our array). 
+# For this reason, hash functions are also known as compression functions.
+# Much like an image that has been shrunk to a lower resolution, the output of a hash function contains less data than the input. 
+# Because of this, hashing is not a reversible process. With just a hash value it is impossible to know for sure the key that was plugged into the hashing function.
